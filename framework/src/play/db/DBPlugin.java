aa14994985da9db106f106e8b061d1bd5a557651
bruno <brunomoreirarocha@gmail.com>
2022-07-12 10:26:30 +0100

    ssl implementation

--- framework/src/play/db/DBPlugin.java
+++ framework/src/play/db/DBPlugin.java
@@ -21,12 +21,27 @@ import java.io.File;
 import java.sql.*;
 import java.util.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 public class DBPlugin extends PlayPlugin {
 
     public static String url = "";
     public static DefaultAWSCredentialsProviderChain creds;
     public static String AWS_ACCESS_KEY;
     public static String AWS_SECRET_KEY;
+
+    private static final String SSL_CERTIFICATE = "rds-ca-2019-root.pem";
+    private static final String KEY_STORE_TYPE = "JKS";
+    private static final String KEY_STORE_PROVIDER = "SUN";
+    private static final String KEY_STORE_FILE_PREFIX = "sys-connect-via-ssl-cacerts";
+    private static final String KEY_STORE_FILE_SUFFIX = ".jks";
+    private static final String DEFAULT_KEY_STORE_PASSWORD = "Jo1nEdUpByBeeL1n3!@#";
    
     protected DataSourceFactory factory(Configuration dbConfig) {
         String dbFactory = dbConfig.getProperty("db.factory", "play.db.hikaricp.HikariDataSourceFactory");
@@ -64,6 +79,7 @@ public class DBPlugin extends PlayPlugin {
 
                     if (dbConfig.getProperty("db.url") != null
                             && dbConfig.getProperty("db.url").toLowerCase().startsWith("jdbc:iampostgresql")) {
+                        setSslProperties();
                         creds = new DefaultAWSCredentialsProviderChain();
                         AWS_ACCESS_KEY = creds.getCredentials().getAWSAccessKeyId();
                         AWS_SECRET_KEY = creds.getCredentials().getAWSSecretKey();
@@ -169,6 +185,69 @@ public class DBPlugin extends PlayPlugin {
                         .build());
 
         return authToken;
+    }
+
+    /**
+     * This method sets the SSL properties which specify the key store file, its type and password:
+     * @throws Exception
+     */
+    private static void setSslProperties() throws Exception {
+        System.setProperty("javax.net.ssl.trustStore", createKeyStoreFile());
+        System.setProperty("javax.net.ssl.trustStoreType", KEY_STORE_TYPE);
+        System.setProperty("javax.net.ssl.trustStorePassword", DEFAULT_KEY_STORE_PASSWORD);
+    }
+
+    /**
+     * This method returns the path of the Key Store File needed for the SSL verification during the IAM Database Authentication to
+     * the db instance.
+     * @return
+     * @throws Exception
+     */
+    private static String createKeyStoreFile() throws Exception {
+        return createKeyStoreFile(createCertificate()).getPath();
+    }
+
+    /**
+     *  This method generates the SSL certificate
+     * @return
+     * @throws Exception
+     */
+    private static X509Certificate createCertificate() throws Exception {
+        CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        URL url = new File(SSL_CERTIFICATE).toURI().toURL();
+        if (url == null) {
+            throw new Exception();
+        }
+        try (InputStream certInputStream = url.openStream()) {
+            return (X509Certificate) certFactory.generateCertificate(certInputStream);
+        }
+    }
+
+    /**
+     * This method creates the Key Store File
+     * @param rootX509Certificate - the SSL certificate to be stored in the KeyStore
+     * @return
+     * @throws Exception
+     */
+    private static File createKeyStoreFile(final X509Certificate rootX509Certificate) throws Exception {
+        File keyStoreFile = File.createTempFile(KEY_STORE_FILE_PREFIX, KEY_STORE_FILE_SUFFIX);
+        try (FileOutputStream fos = new FileOutputStream(keyStoreFile.getPath())) {
+            KeyStore ks = KeyStore.getInstance(KEY_STORE_TYPE, KEY_STORE_PROVIDER);
+            ks.load(null);
+            ks.setCertificateEntry("rootCaCertificate", rootX509Certificate);
+            ks.store(fos, DEFAULT_KEY_STORE_PASSWORD.toCharArray());
+        }
+        return keyStoreFile;
+    }
+
+    /**
+     * This method clears the SSL properties.
+     * @throws Exception
+     */
+    private static void clearSslProperties() throws Exception {
+        System.clearProperty("javax.net.ssl.trustStore");
+        System.clearProperty("javax.net.ssl.trustStoreType");
+        System.clearProperty("javax.net.ssl.trustStorePassword");
     }
 
     protected String testDataSource(DataSource ds) throws SQLException {
